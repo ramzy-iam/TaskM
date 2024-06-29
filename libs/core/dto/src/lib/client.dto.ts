fcd084abb3d11cc938659b90c92b3e82c078c13d
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2024-06-30 00:30:40 +0100

    remove commented code

--- libs/core/dto/src/lib/client.dto.ts
+++ libs/core/dto/src/lib/client.dto.ts
@@ -1,4 +1,4 @@
-import { Expose, Transform, Type } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import {
   IsString,
   IsNotEmpty,
@@ -8,7 +8,6 @@ import {
   IsEmail,
   IsEnum,
   IsPositive,
-  IsNumber,
   IsUppercase,
 } from 'class-validator';
 import { BasicDto, BasicFilterDto } from './basic.dto';
