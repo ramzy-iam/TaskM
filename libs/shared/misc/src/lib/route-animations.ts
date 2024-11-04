e5d1c6be89f541714cb05432055986b134eea501
Ramses Feukeng <ramsesfeukeng@gmail.com>
2024-11-04 11:55:09 +0100

    style:change route transition

--- libs/shared/misc/src/lib/route-animations.ts
+++ libs/shared/misc/src/lib/route-animations.ts
@@ -14,15 +14,9 @@ export const fader = trigger('routeAnimations', [
         left: 0,
         width: '100%',
         opacity: 0,
-        transform: 'scale(0) translateY(100%)',
         padding: '0 2.5rem',
       }),
     ]),
-    query(':enter', [
-      animate(
-        '500ms ease',
-        style({ opacity: 1, transform: 'scale(1) translateY(0)' }),
-      ),
-    ]),
+    query(':enter', [animate('600ms ease', style({ opacity: 1 }))]),
   ]),
 ]);
